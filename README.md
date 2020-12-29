c76f89ae865c708296ebc9dcd595db79feb66e40
Cory Kromer-Edwards <cjedwards1@malone.edu>
2020-12-29 16:11:16 -0600

    Adding readme and autoencoder file

---
+++ README.md
@@ -0,0 +1,2 @@
+# Heuristic Autoencoder
+Using Least Squares calculations to perform heuristic search for an encoder matrix and decoder matrix.
---
+++ autoencoder.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  29 15:53 2020
+
+Build W encoder and Z decoder matrices.
+Then find, and return, Least Squares loss.
+
+@author: Cory Kromer-Edwards
+"""
+
+import numpy as np
+
+def sci(x, w=None):
+    """Run """
+    size = 
+    if w is None:
+        w = np.random.normal(0, 1, (size, size))
