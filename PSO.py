46cd8d73d5a572d6b79ffb4ae967c72ce62866bc
jamil gafur <jamilggafur@gmail.com>
2021-01-04 21:14:45 -0500

    MNST doesnt crash

--- PSO.py
+++ PSO.py
@@ -11,26 +11,12 @@ PSO :
 
         z*phi(w) = output_img => save
 
-
-
-test_random():
-    # Sanity test to make sure that feature number positively impacts least squares error.
-    num_points = 100
-    num_data_per_point = 55
-    x_in = np.random.normal(size=(num_data_per_point, num_points))
-    loss_values = []
-    for num_features in [1, 5, 10, 15, 20, 40, 70]:
-        ae = AutoEncoder(x_in, num_features, random_seed=1234)
-        w_in = np.random.normal(size=(num_data_per_point, num_features))
-        z_out, least_squares_test = ae.psi(w_in)
-        loss_values.append(least_squares_test)
-        print(f"(# features : Least squares error = ({num_features} : {least_squares_test})")
-
-    plotter.plot_loss(loss_values, "Random_Test_with_Features", "Num features from list [1, 5, 10, 15, 20, 40, 70]")
-
 """
 import numpy as np
+import plotter
 from autoencoder import AutoEncoder
+from keras.datasets import mnist
+
 
 class Particle:
     def __init__(self, name, initial_weight, cost_function):
@@ -48,7 +34,7 @@ class Particle:
 
     # evaluate current fitness
     def evaluate(self):
-        _ , self.cost = self.cost_function(self.position)
+        _, self.cost = self.cost_function(self.position)
         # check to see if the current position is an individual best
         if self.err_best == -1 or self.cost < self.err_best:
             self.pos_best = self.position
@@ -56,9 +42,10 @@ class Particle:
 
     # update new particle velocity
     def update_velocity(self, pos_best_g):
-        vel_cognitive = np.multiply( np.multiply(self.c_weight , np.random.random()) ,  np.subtract(self.pos_best , self.position) )
-        vel_social    = np.multiply( np.multiply(self.s_weight , np.random.random()) ,  np.subtract(pos_best_g , self.position))
-        self.velocity = np.add(np.multiply(self.v_weight , self.velocity ),  np.add(vel_cognitive , vel_social))
+        vel_cognitive = np.multiply(np.multiply(self.c_weight, np.random.random()),
+                                    np.subtract(self.pos_best, self.position))
+        vel_social = np.multiply(np.multiply(self.s_weight, np.random.random()), np.subtract(pos_best_g, self.position))
+        self.velocity = np.add(np.multiply(self.v_weight, self.velocity), np.add(vel_cognitive, vel_social))
 
     # update the particle position based off new velocity updates
     def update_position(self):
@@ -66,18 +53,17 @@ class Particle:
 
 
 class Algorithm():
-    def __init__(self, maxiter, num_particles, num_features, num_points, num_data_per_point):
+    def __init__(self, x_in, maxiter, num_particles, num_features, num_data_per_point):
         self.err_best_g = None  # best error for group
         self.pos_best_g = []  # best position for group
         self.num_particles = 3
         self.maxiter = maxiter
         # establish the swarm
-        x_in = np.random.normal(size=(num_data_per_point, num_points))
         ae = AutoEncoder(x_in, num_features, random_seed=1234)
 
         self.swarm = []
         for i in range(0, num_particles):
-            w_in = np.random.random() * np.random.normal(size=(num_data_per_point, num_features), scale=5)
+            w_in = np.random.normal(size=(num_data_per_point, num_features), scale=5)
             self.swarm.append(Particle(i, w_in, ae.psi))
 
     def run(self):
@@ -94,18 +80,36 @@ class Algorithm():
             for particle in self.swarm:
                 particle.update_velocity(self.pos_best_g)
                 particle.update_position()
+            print("{} : {}".format(i, self.err_best_g))
 
         return self.err_best_g
 
 
-
 def test_random():
+    print("===== RUNNING TEST =====")
     maxiter = 100
     num_particles = 10
     num_points = 100
     num_data_per_point = 55
-    for num_features in [1, 10,20]:
-        PSO = Algorithm(maxiter, num_particles, num_features, num_points, num_data_per_point)
+    x_in = np.random.normal(size=(num_data_per_point, num_points))
+
+    for num_features in [1, 10, 20]:
+        PSO = Algorithm( x_in, maxiter, num_particles, num_features, num_data_per_point)
+        least_squares_test = PSO.run()
+        print(f"(# features : Least squares error = ({num_features} : {least_squares_test})")
+
+
+def test_mnist():
+    print("===== RUNNING MNIST =====")
+    (train_x, _), (_, _) = mnist.load_data()
+    plotter.plot_mnist(train_x, "original")  # Show original mnist images
+    num_img, img_dim, _ = train_x.shape  # Get number of images and # pixels per square img
+    mnist_in = np.reshape(train_x, (img_dim * img_dim, num_img))  # Reshape images to match autoencoder input
+
+    maxiter = 100
+    num_particles = 10
+    for num_features in [1, 10, 20]:
+        PSO = Algorithm(mnist_in, maxiter, num_particles, num_features, img_dim * img_dim)
         least_squares_test = PSO.run()
         print(f"(# features : Least squares error = ({num_features} : {least_squares_test})")
 
@@ -113,3 +117,4 @@ def test_random():
 if __name__ == '__main__':
     np.random.seed(1234)
     test_random()
+    test_mnist()
